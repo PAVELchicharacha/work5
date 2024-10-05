cb2ada660b3a52b87663b246ff94fdad20851516
PAVELchicharacha <113702919+PAVELchicharacha@users.noreply.github.com>
2024-10-05 15:32:54 +0200

    Update module2.py

--- my_package/my_package/module2.py
+++ my_package/my_package/module2.py
@@ -3,12 +3,12 @@ from my_package.my_package.module1 import filename_f
 filename_c = 'file_c.txt'#запись
 filename_v = 'file_v.txt'#запись большими буквами
 
-
+#запись
 with open(filename_c,'w',encoding='utf-8')as file_c:
     user_text=input("enter words")
     file_c.write(user_text)
 
-
+#запись большими буквами
 with open(filename_c,'r') as file_c,open(filename_v,'w',encoding='utf-8')as file_v:
     for line in file_c:
         file_v.write(user_text.upper())
